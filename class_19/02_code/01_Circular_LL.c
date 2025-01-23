994d0b0dd38700f1a3cc80151f31c2f346237e20
niharhEAVR <debnathnihar14@gmail.com>
2025-01-23 11:35:00 +0530

    23-jan

--- class_19/02_code/01_Circular_LL.c
+++ class_19/02_code/01_Circular_LL.c
@@ -1,56 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
 
 // Function to insert a node at the end
-void insertAtEnd(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    if (*head == NULL) {
+void insertAtEnd(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (*head == NULL)
+    {
         *head = newNode;
-        newNode->next = *head;  // Point to itself to make it circular
+        newNode->next = *head; // Point to itself to make it circular
         return;
     }
-    struct Node* temp = *head;
-    while (temp->next != *head) {  // Traverse to the last node
+    struct Node *temp = *head;
+    while (temp->next != *head)
+    { // Traverse to the last node
         temp = temp->next;
     }
     temp->next = newNode;
-    newNode->next = *head;  // Make it circular
+    newNode->next = *head; // Make it circular
+}
+
+void insertAtFirst(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+
+    if (*head == NULL)
+    {
+        *head = newNode;
+        newNode->next = *head;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != *head)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void deleteFirst(struct Node**head){
+    if(*head == NULL){
+        printf("Sorry cant possible!\n");
+        return;
+    }
+    struct Node *firstNode = *head;
+
+    struct Node *temp = *head;
+    while(temp->next != *head){
+        temp = temp->next;
+    }
+    temp->next = temp->next->next;
+    *head = temp->next;
+    free(firstNode);    
 }
 
 // Function to display the circular linked list
-void display(struct Node* head) {
-    if (head == NULL) {
+void display(struct Node *head)
+{
+    if (head == NULL)
+    {
         printf("List is empty.\n");
         return;
     }
-    struct Node* temp = head;
-    do {
+    struct Node *temp = head;
+    do
+    {
         printf("%d -> ", temp->data);
         temp = temp->next;
-    } while (temp != head);  // Stop when we reach the starting node
-    printf("(back to head)\n");
+    } while (temp != head); // Stop when we reach the starting node
+    printf("(back to head): %d\n", temp->data);
 }
 
 // Main function
-int main() {
-    struct Node* head = NULL;
+int main()
+{
+    struct Node *head = NULL;
+    deleteFirst(&head);
+
+    insertAtFirst(&head, 90);
 
     // Insert nodes
     insertAtEnd(&head, 10);
+    deleteFirst(&head);
+    display(head);
     insertAtEnd(&head, 20);
     insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+
+    insertAtFirst(&head, 9);
+    display(head);
+
+    deleteFirst(&head);
 
     // Display the list
     display(head);
--- class_19/02_code/02_Doubly_LL.c
+++ class_19/02_code/02_Doubly_LL.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
-    struct Node* prev;
+    struct Node *next;
+    struct Node *prev;
 };
 
 // Function to create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
     newNode->prev = NULL;
@@ -17,60 +19,118 @@ struct Node* createNode(int data) {
 }
 
 // Function to insert a node at the end
-void insertAtEnd(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    if (*head == NULL) {
-        *head = newNode;  // If the list is empty, set head to the new node
+void insertAtEnd(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode; // If the list is empty, set head to the new node
         return;
     }
-    struct Node* temp = *head;
-    while (temp->next != NULL) {  // Traverse to the last node
+    struct Node *temp = *head;
+    while (temp->next != NULL)
+    { // Traverse to the last node
         temp = temp->next;
     }
-    temp->next = newNode;         // Link the last node to the new node
-    newNode->prev = temp;         // Link the new node back to the last node
+    temp->next = newNode; // Link the last node to the new node
+    newNode->prev = temp; // Link the new node back to the last node
 }
 
+void insertAtFirst(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+
+    struct Node *temp = *head;
+    temp->prev = newNode;
+    newNode->next = temp;
+    *head = newNode;
+}
+
+void deleteAtFirst(struct Node** head){
+    struct Node* deleteNode = *head;
+
+    *head = (*head)->next;
+    (*head)->prev = NULL;
+
+    free(deleteNode);
+}
+
+// void deleteAtEnd(struct Node** head){
+//     struct Node *temp = *head;
+//     while (temp->next!=NULL)
+//     {
+//         temp = temp->next;
+//     }
+
+//     struct Node* deleteNode = temp;
+
+//     temp = temp->prev;
+//     temp->next = NULL;
+
+//     while (temp!=NULL)
+//     {
+//         temp = temp->prev;
+//     }
+//     *head = temp;
+//     free(deleteNode);
+    
+// }
+
 // Function to display the list forward
-void displayForward(struct Node* head) {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
+void displayForward(struct Node *head)
+{
+    printf("Forward: ");
+    while (head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
     }
     printf("NULL\n");
 }
 
 // Function to display the list backward
-void displayBackward(struct Node* head) {
-    if (head == NULL) return;
-    struct Node* temp = head;
-    while (temp->next != NULL) {  // Traverse to the last node
-        temp = temp->next;
+void displayBackward(struct Node *head)
+{
+    if (head == NULL)
+        return;
+
+    while (head->next != NULL)
+    { // Traverse to the last node
+        head = head->next;
     }
-    while (temp != NULL) {        // Traverse backward
-        printf("%d -> ", temp->data);
-        temp = temp->prev;
+    printf("Backward: ");
+    while (head != NULL)
+    { // Traverse backward
+        printf("%d -> ", head->data);
+        head = head->prev;
     }
     printf("NULL\n");
 }
 
 // Main function
-int main() {
-    struct Node* head = NULL;
+int main()
+{
+    struct Node *head = NULL;
 
     // Insert nodes
     insertAtEnd(&head, 10);
     insertAtEnd(&head, 20);
     insertAtEnd(&head, 30);
 
+    insertAtFirst(&head,9);
+
+
     // Display the list forward
-    printf("Forward: ");
     displayForward(head);
 
     // Display the list backward
-    printf("Backward: ");
     displayBackward(head);
 
+    deleteAtFirst(&head);
+    displayForward(head);
+    displayBackward(head);
+
+    deleteAtEnd(&head);
+    displayForward(head);
     return 0;
 }
